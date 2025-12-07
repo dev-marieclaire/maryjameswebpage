f578611209f2542be35fb0fa3506e1dcdf7ce576
dev-marieclaire <mail.maryjamesb@gmail.com>
2025-12-06 19:53:15 -0600

    modified:   faq.html 	deleted:    global_style.css 	modified:   index.html 	modified:   scripts/cursor.js 	deleted:    scripts/works.js 	modified:   works.html 	modified:   works_style.css  Untracked files: 	scripts/ouvre.js 	scripts/ouvre.json 	shared_style.css

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,24 +1,10 @@
 // cursor.js
 
-window.onload = function()
+// This is currently disabled due to uncomplete functionality implementation.
+// I will enable it in the foreseable future.
+
+function control_border_brightness()
 {
-    const cursor_bg = document.getElementById("cursor_bg");
-    
-    var content = document.getElementById("content");
-    var content_rect = content.getBoundingClientRect();
-
-    content.style.borderColor = 'rgba(139, 104, 107, 0.48)';
-
-    var angle = getComputedStyle(content).getPropertyValue("--angle");
-
-    onresize = (event) => {
-        content = document.getElementById("content");
-    };
-
-    const content_x = content_rect.left+(content_rect.width/2);
-    const content_y = content_rect.top+(content_rect.height/2);
-    const aspect_ratio = content_rect.width/content_rect.height;
-
     document.body.onpointermove = event =>
     {
         const { clientX, clientY } = event;
@@ -43,13 +29,37 @@ window.onload = function()
 
         if (x_dist >= content_rect.width/2 || x_dist <= (content_rect.width/2)*-1 ||
             y_dist >= content_rect.height/2 || y_dist <= (content_rect.height/2)*-1)
-            content.style.borderColor = 'rgba(139, 104, 107, 0.48)';
+            // content.style.borderColor = 'rgba(139, 104, 107, 0.48)';
+            content.style.borderColor = 'rgba(244, 244, 244, 0.48)';
         else if (alpha >= 120 && alpha <= 240)
         content.animate(
-            { borderColor: 'rgba(139, 104, 107, '+ alpha/300 +')' },
+            // { borderColor: 'rgba(139, 104, 107, '+ alpha/300 +')' },
+            { borderColor: 'rgba(244, 244, 244, '+ alpha/600 +')' },
             { duration: 5, fill: "forwards" }
         )
 
         // console.log(alpha.toString());
     }
+}
+
+window.onload = function()
+{
+    const cursor_bg = document.getElementById("cursor_bg");
+    
+    var content = document.getElementById("content");
+    var content_rect = content.getBoundingClientRect();
+
+    content.style.borderColor = 'rgba(139, 104, 107, 0.48)';
+
+    var angle = getComputedStyle(content).getPropertyValue("--angle");
+
+    onresize = (event) => {
+        content = document.getElementById("content");
+    };
+
+    const content_x = content_rect.left+(content_rect.width/2);
+    const content_y = content_rect.top+(content_rect.height/2);
+    const aspect_ratio = content_rect.width/content_rect.height;
+
+    control_border_brightness();
 }
