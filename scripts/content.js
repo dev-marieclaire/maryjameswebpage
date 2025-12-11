a63b570cdc6bd82f211348ba5b253920357e80b9
dev-marieclaire <mail.maryjamesb@gmail.com>
2025-12-11 00:17:23 -0600

    Introducing the gallery. 	modified:   content.css 	modified:   index.css 	modified:   index.html 	modified:   scripts/content.js

--- scripts/content.js
+++ scripts/content.js
@@ -73,117 +73,163 @@ const im =
     {
         nom: "Paysage montagneux",
         auteur_e: "Robert Delaunay",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/e/e5/Robert_Delaunay_Paysage_montagneux_1904.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/e/e5/Robert_Delaunay_Paysage_montagneux_1904.jpg"
     },
     {
         nom: "Water Lilies and Japanese Bridge",
         auteur_e: "Claude Monet",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/The_Water-Lily_Pond_-_Google_Arts_%26_Culture.jpg/1043px-The_Water-Lily_Pond_-_Google_Arts_%26_Culture.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/The_Water-Lily_Pond_-_Google_Arts_%26_Culture.jpg/1043px-The_Water-Lily_Pond_-_Google_Arts_%26_Culture.jpg"
     },
     // The one used in "The Snow is Dancing" is pending.
     // The one used in "Microtonal Miniatures" is pending.
     {
         nom: "Bridge Over a River",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Santiago_Rusi%C3%B1ol_-_Bridge_over_a_River_-_Google_Art_Project.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Santiago_Rusi%C3%B1ol_-_Bridge_over_a_River_-_Google_Art_Project.jpg"
     },
     {
         nom: "Garden of Montmartre",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/7/73/Santiago_Rusi%C3%B1ol_-_Garden_of_Montmartre_-_Google_Art_Project.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/7/73/Santiago_Rusi%C3%B1ol_-_Garden_of_Montmartre_-_Google_Art_Project.jpg"
     },
     {
         nom: "The Daughters of Edward Darley Boit",
         auteur_e: "John Singer Sargent",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/9/96/The_Daughters_of_Edward_Darley_Boit%2C_John_Singer_Sargent%2C_1882_%28unfree_frame_crop%29.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/9/96/The_Daughters_of_Edward_Darley_Boit%2C_John_Singer_Sargent%2C_1882_%28unfree_frame_crop%29.jpg"
     },
     {
         nom: "Les Glacons",
         auteur_e: "Claude Monet",
-        lier: "https://commons.wikimedia.org/wiki/File:Claude_Monet_-_Les_Glacons.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/b/b9/Claude_Monet_-_Les_Glacons.jpg"
     },
     // The one used in "Suite no. 0: 1st movement" is pending.
     {
         nom: "Muralla Verde",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/8/81/Rusi%C3%B1ol_en_MNCARS.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/8/81/Rusi%C3%B1ol_en_MNCARS.jpg"
     },
     {
         nom: "Carnation, Lily, Lily, Rose",
         auteur_e: "John Singer Sargent",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/e/ed/John_Singer_Sargent_-_Carnation%2C_Lily%2C_Lily%2C_Rose_-_Google_Art_Project.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/e/ed/John_Singer_Sargent_-_Carnation%2C_Lily%2C_Lily%2C_Rose_-_Google_Art_Project.jpg"
     },
     {
         nom: "Glorieta al Atardecer",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/1/14/Santiago_Rusi%C3%B1ol_%2C1913_-_Glorieta_al_atardecer%2C_Oil_on_canvas%2C_82x99_cm.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/1/14/Santiago_Rusi%C3%B1ol_%2C1913_-_Glorieta_al_atardecer%2C_Oil_on_canvas%2C_82x99_cm.jpg"
     },
     {
         nom: "Carrer de Rouen",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/3/34/Carrer_de_Rouen.JPG"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/3/34/Carrer_de_Rouen.JPG"
     },
     {
         nom: "Son Moragues",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/c/c0/Santiago_Rusi%C3%B1ol-Son_Moragues.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/c/c0/Santiago_Rusi%C3%B1ol-Son_Moragues.jpg"
     },
     {
         nom: "The Spreading",
         auteur_e: "George Clausen",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/d/d2/George_Clausen_-_The_spreading_tree.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/d/d2/George_Clausen_-_The_spreading_tree.jpg"
     },
     {
         nom: "The Landing Stage",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Santiago_Rusi%C3%B1ol_-_The_Landing_Stage_-_Google_Art_Project.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Santiago_Rusi%C3%B1ol_-_The_Landing_Stage_-_Google_Art_Project.jpg"
     },
     {
         nom: "The River Seine at la Grande-jatte",
         auteur_e: "Georges Seurat",
-        lier: "https://uploads6.wikiart.org/images/georges-seurat/the-river-seine-at-la-grande-jatte-1888.jpg"
+        lien: "https://uploads6.wikiart.org/images/georges-seurat/the-river-seine-at-la-grande-jatte-1888.jpg"
     },
     {
         nom: "Camino de Rosas",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/5/54/Camino_de_rosas_-_Santiago_Rusi%C3%B1ol.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/5/54/Camino_de_rosas_-_Santiago_Rusi%C3%B1ol.jpg"
     },
     {
         nom: "Satie Logis",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/e/ec/Satie_Logis%2C_Santiago_Rusinol%2C_1891.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/e/ec/Satie_Logis%2C_Santiago_Rusinol%2C_1891.jpg"
     },
     {
         nom: "La Butte",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/3/34/Santiago_Rusinol_La_Butte.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/3/34/Santiago_Rusinol_La_Butte.jpg"
     },
     {
         nom: "Female Figure",
         auteur_e: "Santiago Rusiñol",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/0/08/%28Barcelona%29_Figura_femenina_-_Santiago_Rusi%C3%B1ol_-_Museu_Nacional_d%27Art_de_Catalunya.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/0/08/%28Barcelona%29_Figura_femenina_-_Santiago_Rusi%C3%B1ol_-_Museu_Nacional_d%27Art_de_Catalunya.jpg"
     },
     {
         nom: "Le Bohemi",
         auteur_e: "Ramón Casas",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/0/0a/El_bohemi_by_Ramon_Casas.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/0/0a/El_bohemi_by_Ramon_Casas.jpg"
     },
     {
         nom: "Impression, Sunrise",
         auteur_e: "Claude Monet",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/5/59/Monet_-_Impression%2C_Sunrise.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/5/59/Monet_-_Impression%2C_Sunrise.jpg"
     },
     {
         nom: "Woman with a Parasol / Madame Monet and Her Son",
         auteur_e: "Claude Monet",
-        lier: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Claude_Monet_-_Woman_with_a_Parasol_-_Madame_Monet_and_Her_Son_-_Google_Art_Project.jpg"
+        lien: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Claude_Monet_-_Woman_with_a_Parasol_-_Madame_Monet_and_Her_Son_-_Google_Art_Project.jpg"
     }
     // {
     //     nom: ,
     //     auteur_e: "",
-    //     lier: ""
+    //     lien: ""
     // },
 ]
+
+/*const max_imWidth = 640, max_imHeight = 640;
+
+function compressImage(inputImage)
+{
+    const file = inputImage.src;
+    const blobURL = URL.createObjectURL(file);
+    const outputImage = new Image();
+
+    outputImage = blobURL;
+    outputImage.onload = get_newImage(outputImage);
+}
+
+function get_newImage(im)
+{
+    
+    const [nouveau_w, nouveau_h] = calc_imgSize(im);
+    
+    const canevas = document.createElement("canvas");
+    canevas.width = nouveau_w; canevas.height = nouveau_h;
+
+    const ctx = canevas.getContext("2d");
+    ctx.drawImage(im, 0, 0, nouveau_w, nouveau_h);
+}
+
+function calc_imgSize(im)
+{
+    let w = im.width; let h = im.height;
+
+    if (w > h && w > max_imWidth)
+    {
+        h = Math.round(
+            (h*max_imWidth)/w
+        );
+        w = max_imWidth;
+    }
+    else if (h > w && w > max_imWidth)
+    {
+        w = Math.round(
+            (w*max_imHeight)/h
+        );
+        h = max_imHeight;
+    }
+
+    return [w, h];
+}*/
 
 window.onload = function()
 {
@@ -199,19 +245,19 @@ window.onload = function()
     {
         if (event.target.id == "faq_button")
         {
-            im_container.innerHTML = ouvre_container.innerHTML = '';
+            txt_container.innerText = im_container.innerHTML = ouvre_container.innerHTML = '';
             current_text = text[1];
             txt_container.innerText = current_text.txt_container+current_text.prolongation;
         }
         if (event.target.id == "home_button")
         {
-            im_container.innerHTML = ouvre_container.innerHTML = '';
+            txt_container.innerText = im_container.innerHTML = ouvre_container.innerHTML = '';
             current_text = text[0];
             txt_container.innerText = current_text.txt_container+current_text.prolongation;
         }
         if (event.target.id == "gallery_button")
         {
-            im_container.innerHTML = ouvre_container.innerHTML = '';
+            txt_container.innerText = im_container.innerHTML = ouvre_container.innerHTML = '';
             for (i = 0; i<im.length; i++)
             {
                 let im_item = document.createElement("img");
@@ -246,9 +292,12 @@ window.onload = function()
                 let item_stream = document.createElement("a");
                 let item_partition = document.createElement("a");
 
+                // compressImage(item_couverture);
+
                 // Introduces the title and cover information on their respective HTML element.
                 item_nom.innerText = ouvre[0][i].nom;
                 item_couverture.src = ouvre[0][i].couverture;
+                item_couverture.alt = "error.";
 
                 // In case the work is unfinished but anounced (aka. WIP),
                 // the stream and sheet music download links are set to null.
@@ -266,7 +315,7 @@ window.onload = function()
                 }
 
                 // if (ouvre[0][i].stream_lien != null && ouvre[0][i].partition_lien != null)
-                //     partition_item.innerText = "Comming soon.";
+                //     item_partition.innerText = "Comming soon.";
 
                 // if the item needs a container for appropiate align, it gets added to a child element first.
                 couverture_container.appendChild(item_couverture);
